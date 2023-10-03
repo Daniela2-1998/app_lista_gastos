55a00bfe0d73ed9e4a94d6e0a35e992ea929bb53
Daniela2-1998 <danimansilla27@gmail.com>
2023-10-03 13:09:08 -0300

    función agregar gasto agregada al formulario

--- src/componentes/FormularioGasto.js
+++ src/componentes/FormularioGasto.js
@@ -4,6 +4,11 @@ import Boton from '../elementos/Boton';
 import {ReactComponent as IconoPlus} from '../imagenes/plus.svg';
 import SelectCategorias from './SelectCategorias';
 import DatePicker from './DatePicker';
+import agregarGasto from '../firebase/agregarGasto';
+import getUnixTime from 'date-fns/getUnixTime';
+import fromUnixTime from 'date-fns/fromUnixTime';
+import {useAuth} from '../contextos/AuthContext';
+import Alerta from '../componentes/Alerta';
 
 const FormularioGasto = () => {
 
@@ -16,6 +21,13 @@ const FormularioGasto = () => {
     // estado para el DayPicker
     const [fecha, cambiarFecha] = useState(new Date());
 
+    // obtener usuario
+    const {usuario} = useAuth();
+
+    // alertas
+    const [estadoAlerta, cambiarEstadoAlerta] = useState(false);
+    const [alerta, cambiarAlerta] = useState({});
+
     const handleChange = (e) => {
         if(e.target.name === 'descripcion'){
             cambiarInputDescripcion(e.target.value);
@@ -26,8 +38,55 @@ const FormularioGasto = () => {
     }
 
 
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let cantidad = parseFloat(inputCantidad).toFixed(2);
+        
+        // comprobación de que haya descripción y valor
+        if (inputDescripcion !== '' && inputCantidad !== '') {
+
+            if (cantidad) {
+                agregarGasto({
+                    categoria: categoria,
+                    descripcion: inputDescripcion,
+                    cantidad: cantidad,
+                    fecha: getUnixTime(fecha),
+                    uidUsuario: usuario.uid
+                })
+                // devuelve la promesa de la función agregar gasto
+                // reiniciamos valores
+                .then(() => {
+                    cambiarCategoria('hogar');
+                    cambiarInputDescripcion('');
+                    cambiarInputCantidad('');
+                    cambiarFecha(new Date());
+
+                    cambiarEstadoAlerta(true);
+                    cambiarAlerta({ tipo: 'exito', mensaje: 'El gasto se agregó correctamente.' });
+                })
+                .catch((error) => {
+                    cambiarEstadoAlerta(true);
+                    cambiarAlerta({ tipo: 'error', mensaje: 'Hubo un problema al intentar agregar tu gasto.' });
+                    console.log(error);
+                });
+
+            } else {
+                cambiarEstadoAlerta(true);
+                cambiarAlerta({ tipo: 'error', mensaje: 'El valor ingresado no es correcto.' });
+            }
+
+        } else {
+            cambiarEstadoAlerta(true);
+            cambiarAlerta({ tipo: 'error', mensaje: 'Por favor completa todos los campos.' });
+        }
+        
+
+    }
+
     return (  
-        <Formulario>
+        <Formulario onSubmit={handleSubmit}>
             <ContenedorFiltros>
                 <SelectCategorias
                     categoria={categoria}
@@ -59,11 +118,17 @@ const FormularioGasto = () => {
             </div>
 
             <ContenedorBoton>
-                <Boton as="button" primario conIcono> 
+                <Boton as="button"> 
                     Agregar gasto
                     <IconoPlus />
                 </Boton>
             </ContenedorBoton>
+            <Alerta
+                tipo={alerta.tipo}
+                mensaje={alerta.mensaje}
+                estadoAlerta={estadoAlerta}
+                cambiarEstadoAlerta={cambiarEstadoAlerta}
+            />
         </Formulario>
     );
 }
